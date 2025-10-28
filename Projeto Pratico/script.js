69b99c59c61f6a9ddc54687be26a6d962f3b58de
Jhon <jhoncavalli@hotmail.com>
2025-10-28 17:04:25 -0300

    adiciona funcionalidade de conversão de moeda e estiliza input

--- Projeto Pratico/script.js
+++ Projeto Pratico/script.js
@@ -1,1 +1,12 @@
-const 
+const convertButton = document .querySelector('#converter');
+
+
+function convertValues() {
+    const inputCurrencyValue = document .querySelector('.input-convert').value;
+
+    const dolarValue = 5.2;
+    
+}
+
+
+convertButton.addEventListener('click', convertValues )
