627606cbfebd1642f0b58fd7d1ff04f07c9d21de
Jhon <jhoncavalli@hotmail.com>
2025-10-28 19:12:44 -0300

    foi alterado arquivos no HTML  e CSS e feitos animaçoês em alguns pontos. e efeito alteraçoes no JS.

--- Projeto Pratico/script.js
+++ Projeto Pratico/script.js
@@ -3,9 +3,26 @@ const convertButton = document .querySelector('#converter');
 
 function convertValues() {
     const inputCurrencyValue = document .querySelector('.input-convert').value;
+    const moedaValor = document .querySelector('.moeda-valor'); // Valor em real
+    const moedaValorConvertido = document .querySelector('.moeda-valor-convertido'); // outras moedas
 
     const dolarValue = 5.2;
-    
+    const euroValue = 6.2;
+
+    const convertedValue = inputCurrencyValue / dolarValue;
+
+
+    moedaValor.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style:"currency",
+        currency:"BRL"
+    }).format(inputCurrencyValue);
+
+    moedaValorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
+        style:"currency",
+        currency:"USD"
+    }).format(convertedValue);
+
+
 }
 
 
