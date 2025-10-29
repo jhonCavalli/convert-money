7d6283e8532b3f3887c47797439d649eb3e318bb
Jhon <jhoncavalli@hotmail.com>
2025-10-29 00:17:39 -0300

    alterado arquivo HTML, JS e add codigo e excluido codigo, codigo dando erro na validação.

--- Projeto Pratico/script.js
+++ Projeto Pratico/script.js
@@ -1,4 +1,5 @@
 const convertButton = document .querySelector('#converter');
+const convertSelect = document .querySelector('.valores-moedas');
 
 
 function convertValues() {
@@ -6,24 +7,63 @@ function convertValues() {
     const moedaValor = document .querySelector('.moeda-valor'); // Valor em real
     const moedaValorConvertido = document .querySelector('.moeda-valor-convertido'); // outras moedas
 
+
+    console.log(convertSelect.value)
+
+
     const dolarValue = 5.2;
     const euroValue = 6.2;
 
-    const convertedValue = inputCurrencyValue / dolarValue;
 
+
+    if(convertSelect.value === "dolar") { // SE 0 select estiver selecionado o valor de dolar, ele vai pegar o valor de dolar para converter.
+        moedaValorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
+        style:"currency",
+        currency:"USD"
+    }).format(inputCurrencyValue / dolarValue);
+
+    }
+
+
+    if(convertSelect.value === "euro") { // SE 0 select estiver selecionado o valor de euro, ele vai pegar o valor de euro para converter.
+        moedaValorConvertido.innerHTML = new Intl.NumberFormat("en-DE", {
+            style:"currency",
+            currency:"EUR"
+        }).format(inputCurrencyValue / euroValue);
+
+    }
 
     moedaValor.innerHTML = new Intl.NumberFormat("pt-BR", {
         style:"currency",
         currency:"BRL"
     }).format(inputCurrencyValue);
 
-    moedaValorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
-        style:"currency",
-        currency:"USD"
-    }).format(convertedValue);
-
-
+    
 }
 
+
+function changeCurrency() {
+ const moedaName = document .getElementById('moeda-name');
+ const moedaImg = document .querySelector('.moeda-img');
+
+    if (convertSelect.value === 'dolar') {
+         moedaName.innerHTML = 'Dólar Americano';
+         moedaImg.src = './assets/img/Dolar.png'
+    }
+       
+
+    if (convertSelect.value === 'euro') {
+         moedaName.innerHTML = 'Euro';
+         moedaImg.src = './assets/img/Euro.png'
+    }
+
+    convertValues();
+    
+}
+
+
+   
+
+convertSelect.addEventListener('change', changeCurrency);
 
 convertButton.addEventListener('click', convertValues )
